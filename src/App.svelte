a2b3880fb97a607a3d25e7126bcb259be6d740d7
bmehder <bmehder@gmail.com>
2022-07-18 16:13:11 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<header>
+  <p>
+    Looking for Restore Wound Care LLC in Colorado? <a href="https://restorewoundcare.com/"
+      >Please Click Here.</a
+    >
+  </p>
+  <p>
+    <a href="https://restorewoundcare.com/"
+      ><img src="restore-wound-care-logo.png" alt="Restore Wound Care" /></a
+    >
+  </p>
+  <p>Our new web address is <a href="https://restorewoundcare.com/">www.restorewoundcare.com</a></p>
+</header>
+
+<main>
+  <p>
+    Welcome to the home for the new Restore Medical Group – offering groundbreaking treatments in
+    mobile healthcare.
+  </p>
+  <p>Our new website is launching in August of 2022. Please stay tuned.</p>
+</main>
+
+<style>
+  header,
+  main {
+    padding: 4rem 2rem;
+    font-size: 1.5rem;
+    text-align: center;
+  }
+  header {
+    background-color: #19f;
+  }
+  main {
+    max-width: 420px;
+    margin: auto;
+  }
+  img {
+    display: block;
+    width: 300px;
+    margin: auto;
+  }
+  p {
+    margin-bottom: 1rem;
+  }
+  a {
+    color: white;
+  }
+</style>
